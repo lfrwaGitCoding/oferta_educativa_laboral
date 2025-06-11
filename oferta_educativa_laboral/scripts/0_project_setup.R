1525824a76b8d3d9bed4fb745f34aeec6218d390
antoniojbt <5346783+antoniojbt@users.noreply.github.com>
2025-06-11 17:13:38 -0600

    Consolidate scripts directory

--- oferta_educativa_laboral/scripts/0_project_setup.R
+++ oferta_educativa_laboral/scripts/0_project_setup.R
@@ -1,1 +1,5 @@
-/Users/antoniob/Documents/work/science/devel/github/antoniojbt/project_tools/project_tools/0_project_setup.R
+# This file was previously a symlink to a helper script from the
+# `project_tools` repository. It is not included here.
+# To run the analysis you need to obtain `0_project_setup.R` from
+# https://github.com/antoniojbt/project_tools (or your local clone)
+# and place it at `scripts/0_project_setup.R`.
--- oferta_educativa_laboral/scripts/grep_many_terms.sh
+++ oferta_educativa_laboral/scripts/grep_many_terms.sh
@@ -1,1 +1,4 @@
-/Users/antoniob/Documents/work/science/devel/github/antoniojbt/project_tools/project_tools/grep_many_terms.sh
+#!/bin/bash
+# Placeholder for the original `grep_many_terms.sh` script which lived in the
+# `project_tools` repository. Download it from
+# https://github.com/antoniojbt/project_tools and place it here if needed.

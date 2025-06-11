3eff30d2ad7f9c55c07e7d579de787cc81c7c367
antoniojbt <5346783+antoniojbt@users.noreply.github.com>
2025-06-11 17:15:23 -0600

    Delete oferta_educativa_laboral/scripts/0_project_setup.R

--- oferta_educativa_laboral/scripts/0_project_setup.R
+++
@@ -1,5 +0,0 @@
-# This file was previously a symlink to a helper script from the
-# `project_tools` repository. It is not included here.
-# To run the analysis you need to obtain `0_project_setup.R` from
-# https://github.com/antoniojbt/project_tools (or your local clone)
-# and place it at `scripts/0_project_setup.R`.

dc70e1fef9f481ba9ea168d4842e4ce606fb6c7c
antoniojbt <5346783+antoniojbt@users.noreply.github.com>
2025-08-06 11:13:07 -0600

    Refactor some_script for coverage

--- tests/testthat/test_some_script.R
+++ tests/testthat/test_some_script.R
@@ -1,17 +1,14 @@
 library(testthat)
 library(withr)
 
-# Basic smoke test for scripts/some_script.R
-
-# script <- file.path("..", "..", "scripts", "some_script.R")
 script_loc <- testthat::test_path("..", "..", "scripts", "some_script.R")
+source(script_loc)
 
-test_that("some_script.R executes and creates output", {
+test_that("add_ratio executes and creates output", {
   in_csv <- local_tempfile(fileext = ".csv")
   out_csv <- local_tempfile(fileext = ".csv")
   write.csv(data.frame(a = 1:2, b = 2:3), in_csv, row.names = FALSE)
-  exit_code <- system2("Rscript", c(script_loc, in_csv, out_csv))
-  expect_equal(exit_code, 0)
+  add_ratio(in_csv, out_csv)
   expect_true(file.exists(out_csv))
 })
 
@@ -19,7 +16,7 @@ test_that("output has expected columns", {
   in_csv <- local_tempfile(fileext = ".csv")
   out_csv <- local_tempfile(fileext = ".csv")
   write.csv(data.frame(a = 1:2, b = 2:3), in_csv, row.names = FALSE)
-  system2("Rscript", c(script_loc, in_csv, out_csv))
+  add_ratio(in_csv, out_csv)
   out <- read.csv(out_csv)
   expect_equal(names(out), c("a", "b", "new_var"))
   expect_type(out$new_var, "double")
@@ -30,35 +27,7 @@ test_that("new_var column contains ratio of first two columns", {
   out_csv <- local_tempfile(fileext = ".csv")
   df <- data.frame(a = c(2, 4), b = c(1, 2))
   write.csv(df, in_csv, row.names = FALSE)
-  system2("Rscript", c(script_loc, in_csv, out_csv))
+  add_ratio(in_csv, out_csv)
   out <- read.csv(out_csv)
   expect_equal(out$new_var, df$a / df$b)
 })
-
-#test_that("missing input triggers error", {
-#  missing <- local_tempfile(fileext = ".csv")
-#  out_csv <- local_tempfile(fileext = ".csv")
-#  if (file.exists(missing)) file.remove(missing)
-#  output <- system2(
-#    "Rscript",
-#    c(script_loc, missing, out_csv),
-#    stderr = TRUE,
-#    stdout = TRUE
-#  )
-#  exit_code <- attr(output, "status")
-#  expect_true(exit_code != 0)
-#  expect_match(
-#    paste(output, collapse = "\n"),
-#    "cannot open|Usage: some_script\\.R <input_csv> <output_csv>"
-#  )
-#})
-
-#test_that("missing arguments triggers usage error", {
-#  output <- system2("Rscript", script_loc, stderr = TRUE, stdout = TRUE)
-#  exit_code <- attr(output, "status")
-#  expect_true(exit_code != 0)
-#  expect_match(
-#    paste(output, collapse = "\n"),
-#    "Usage: some_script\\.R <input_csv> <output_csv>"
-#  )
-#})
